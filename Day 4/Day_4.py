2778c004eae177efd495ea93db72d3e16253fb45
Shiven Taneja <shiven.taneja@gmail.com>
2025-01-26 18:24:05 +0000

    Day 4 part 1

---
+++ Day 4/Day_4.py
@@ -0,0 +1,61 @@
+def xmas_forward(file, i, j):
+    if j + 3 <= len(file[i]):
+        if file[i][j+1] == 'M' and file[i][j+2] == 'A' and file[i][j+3] == "S":
+            return 1 
+    return 0
+
+def xmas_backward(file, i, j):
+    if j >= 3:
+        if file[i][j-1] == 'M' and file[i][j-2] == 'A' and file[i][j-3] == "S":
+            return 1
+    return 0
+
+def xmas_up(file, i, j):
+    if i >= 3:
+        if file[i-1][j] == 'M' and file[i-2][j] == 'A' and file[i-3][j] == "S":
+            return 1
+    return 0
+
+def xmas_down(file, i, j):
+    if i + 3 < len(file):
+        if file[i+1][j] == 'M' and file[i+2][j] == 'A' and file[i+3][j] == "S":
+            return 1
+    return 0
+
+def xmas_diag(file, i, j):
+    c = 0
+    #diag right down
+    if i + 3 < len(file) and j + 3 <= len(file[i]):
+        if file[i+1][j+1] == 'M' and file[i+2][j+2] == 'A' and file[i+3][j+3] == "S":
+            c += 1
+    #diag left down
+    if i + 3 < len(file) and j >= 3:
+        if file[i+1][j-1] == 'M' and file[i+2][j-2] == 'A' and file[i+3][j-3] == "S":
+            c += 1
+
+    #diag right up
+    if i >= 3 and j + 3 <= len(file[i]):
+        if file[i-1][j+1] == 'M' and file[i-2][j+2] == 'A' and file[i-3][j+3] == "S":
+            c += 1
+    #diag left up
+    if i >= 3 and j >= 3:
+        if file[i-1][j-1] == 'M' and file[i-2][j-2] == 'A' and file[i-3][j-3] == "S":
+            c += 1
+            
+    return c
+
+# Part 1 
+if __name__ == '__main__':
+    file = open("Day 4/input.txt").readlines()
+    # file = ["MMMSXXMASM", "MSAMXMSMSA", "AMXSXMAAMM", "MSAMASMSMX", "XMASAMXAMM", "XXAMMXXAMA", "SMSMSASXSS", "SAXAMASAAA", "MAMMMXMMMM", "MXMXAXMASX"]
+    count = 0
+    for i in range(len(file)): 
+        for j in range(len(file[i])):
+            if file[i][j] == 'X':
+                count += xmas_forward(file, i, j) 
+                count += xmas_backward(file, i, j)
+                count += xmas_up(file, i, j)
+                count += xmas_down(file, i, j)
+                count += xmas_diag(file, i, j)
+    print(count)
+                
